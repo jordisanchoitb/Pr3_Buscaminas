ddafdaab2a208128640e4f3cdd35735f8e5b20cb
jordisanchoitb <jordi.sancho.7e7@itb.cat>
2024-03-21 22:07:23 +0100

    cambiado formateo del formulario

--- src/js/formbuscaminas.js
+++ src/js/formbuscaminas.js
@@ -2,19 +2,60 @@ export function init() {
     let buttonPrincipiante = document.getElementById("principiante");
     let buttonIntermedio = document.getElementById("intermedio");
     let buttonExperto = document.getElementById("experto");
+    let errorMinas = document.getElementById("errorMinas");
     buttonPrincipiante.addEventListener("click", () => preset(1));
     buttonIntermedio.addEventListener("click", () => preset(2));
     buttonExperto.addEventListener("click", () => preset(3));
+
+    // Sirve para que se vaya actualizando el error de minas a medida que se cambian los valores de filas, columnas y minas
+    document.getElementById("Minas").addEventListener("input", function () {
+        let filas = document.getElementById("Filas").value;
+        let columnas = document.getElementById("Columnas").value;
+        let minas = document.getElementById("Minas").value;
+        if (filas * columnas <= minas) {
+            errorMinas.textContent = "El número de minas no puede igual o mayor que el número de celdas";
+            errorMinas.className = "error active";
+        } else {
+            errorMinas.textContent = "";
+            errorMinas.className = "error";
+        }
+    });
+
+    document.getElementById("Filas").addEventListener("input", function () {
+        let filas = document.getElementById("Filas").value;
+        let columnas = document.getElementById("Columnas").value;
+        let minas = document.getElementById("Minas").value;
+        if (filas * columnas <= minas) {
+            errorMinas.textContent = "El número de minas no puede igual o mayor que el número de celdas";
+            errorMinas.className = "error active";
+        } else {
+            errorMinas.textContent = "";
+            errorMinas.className = "error";
+        }
+    });
+
+    document.getElementById("Columnas").addEventListener("input", function () {
+        let filas = document.getElementById("Filas").value;
+        let columnas = document.getElementById("Columnas").value;
+        let minas = document.getElementById("Minas").value;
+        if (filas * columnas <= minas) {
+            errorMinas.textContent = "El número de minas no puede igual o mayor que el número de celdas";
+            errorMinas.className = "error active";
+        } else {
+            errorMinas.textContent = "";
+            errorMinas.className = "error";
+        }
+    });
 
     document.getElementById("formbuscaminas").addEventListener("submit", function (event) {
         event.preventDefault();
         let filas = document.getElementById("Filas").value;
         let columnas = document.getElementById("Columnas").value;
         let minas = document.getElementById("Minas").value;
-
+        
         // Validacion para que el numero de minas no sea mayor o igual que el numero de celdas
+        // i que en caso de que este mal no se envie el formulario
         if (filas * columnas <= minas) {
-            alert("El número de minas no puede igual o mayor que el número de celdas");
             return;
         }
         
--- src/js/formconfing.js
+++ src/js/formconfing.js
@@ -1,6 +1,24 @@
 export function init() {
+    let errorTelef = document.getElementById("errorTelef");
+
+    // Sirve para que se vaya actualizando el error del telefono a medida que se cambia el valor
+    document.getElementById("Telefono").addEventListener("input", function () {
+        if (!(RegExp(/^[6-9][0-9]{2} [0-9]{3} [0-9]{3}|^[6-9][0-9]{2} [0-9]{2} [0-9]{2} [0-9]{2}/).test(document.getElementById("Telefono").value))) {
+            errorTelef.textContent = "Introduzca un el telefono en el formato correcto (6|7|8|9)XX XXX XXX o (6|7|8|9)XX XX XX XX";
+            errorTelef.className = "error active";
+        } else {
+            errorTelef.textContent = "";
+            errorTelef.className = "error";
+        }
+    });
+        
     document.getElementById("formconfing").addEventListener("submit", function (event) {
         event.preventDefault();
+        // Evita que se envie el formulario si el telefono no esta en el formato correcto
+        if (!(RegExp(/^[6-9][0-9]{2} [0-9]{3} [0-9]{3}|^[6-9][0-9]{2} [0-9]{2} [0-9]{2} [0-9]{2}/).test(document.getElementById("Telefono").value))) {
+            return;
+        }
+
         CreateCookie("Nombre", document.getElementById("Nombre").value);
         CreateCookie("Apellido", document.getElementById("Apellido").value);
         CreateCookie("Nick", document.getElementById("Nick").value);
